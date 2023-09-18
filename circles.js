5943b10a5d5cfc2ea5fcf334431eeefb26b897cd
Kristians Danilovs <k.danilovs2905@gmail.com>
2023-09-18 17:01:58 +0200

    board html css

---
+++ circles.js
@@ -0,0 +1,17 @@
+
+function Insert(square) {
+    
+    square.innerHTML = '<img src="assets/circle.png" alt="Circle Image">';
+  }
+
+function isAvailableForCircle(square){
+    if(square.innerHTML == '<img src="assets/cross.png">')
+    return false;
+}
+
+function insertCircle(){
+
+}
+
+
+
